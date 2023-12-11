0f80f7386ac34e49c5518bdce92c0002c175e820
M64GitHub <mario.a.schallner@gitub.com>
2023-12-11 03:49:55 +0100

    added preperations for effects application

--- tsprites.cpp
+++ tsprites.cpp
@@ -8,6 +8,9 @@ TSprite::TSprite()
     w = h = x = y = z = 0;
     shadow_map = 0;
     color_map = 0;
+    shadow_map_source = 0;
+    color_map_source = 0;
+    maps_initialized = 0;
 
     // initialize lines array
     for(int l=0; l<128; l++) lines[l] = 0;
@@ -18,13 +21,17 @@ TSprite::TSprite(int ww, int hh)
     x = y = z = 0;
     shadow_map = 0;
     color_map = 0;
-
-    w = ww;
-    h = hh;
+    shadow_map_source = 0;
+    color_map_source = 0;
+    maps_initialized = 0;
 
     for(int l=0; l<h; l++) lines[l] = 0;
 
-    if(!malloc_maps()) { w = 0; h = 0; return; }
+    if(!malloc_maps()) return; 
+
+    // only on success
+    w = ww;
+    h = hh;
 }
 
 TSprite::TSprite(char *imgstr)
@@ -46,6 +53,17 @@ int TSprite::ImportFromFile(char *fn)
     return 0;
 }
 
+void TSprite::Print()
+{
+}
+
+void TSprite::Reset()
+{
+    if(!maps_initialized) return;
+
+
+}
+
 int TSprite::malloc_maps()
 {
     shadow_map = (char *) calloc(w * h, 1);
@@ -54,16 +72,34 @@ int TSprite::malloc_maps()
     color_map = (char *) calloc(w * h * 3, 1); // r, g, b
     if(!color_map) return 1;
 
+    shadow_map_source = (char *) calloc(w * h, 1);
+    if(!shadow_map_source) return 1;
+
+    color_map_source = (char *) calloc(w * h * 3, 1); // r, g, b
+    if(!color_map_source) return 1;
+
+    maps_initialized = 1;
+
     return 0;
 }
 
 int TSprite::free_maps()
 {
+    maps_initialized = 0;
+
+    if(s_source) free(s_source);
+
     if(!shadow_map) return 1;
     free(shadow_map);
 
     if(!color_map) return 1;
     free(color_map);
+
+    if(!shadow_map_source) return 1;
+    free(shadow_map);
+
+    if(!color_map_source) return 1;
+    free(color_map_source);
 
     return 0;
 }
--- tsprites.h
+++ tsprites.h
@@ -14,6 +14,9 @@ public:
     int ImportFromFile(char *fn);
     int ImportFromColorMap(char *cmap);
 
+    void Reset(); // restores original state (after 
+                  // possible applied effects
+
     void Print();
 
     // main attributes w/o getters for fastest access
@@ -26,13 +29,19 @@ public:
     int z = 0;
 
     char *shadow_map; // w * h
-    char *color_map;
+    char *color_map;  // w * h: r, g, b
     char *lines[128]; // more than enough
 
 private:
     int malloc_maps();
     int free_maps();
-    
+
+    // copies of initial state
+    char *s_source;
+    char *shadow_map_source;
+    char *color_map_source;
+    int maps_initialized = 0; // either all or none,
+                              // for better readibility          
 };
 
 #endif
