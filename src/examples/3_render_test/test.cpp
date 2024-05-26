7f534c4f9143205190c5bc940aa4b10daec25d57
M64GitHub <m64.overdrive@gmail.com>
2024-05-26 20:54:00 +0200

    Updated example 3

--- src/examples/3_render_test/test.cpp
+++ src/examples/3_render_test/test.cpp
@@ -11,10 +11,14 @@
 #include <unistd.h>   // for usleep()
 
 int main(int argc, char **argv) {
+
+  // NOTE: --------------------------------------------------- VARS / SETUP ---
+
+  FPS = 60;
+
   struct timeval tv;
   unsigned long ts1 = 0;
   unsigned long ts2 = 0;
-  FPS = 60;
   fps_in_us = fps_to_us(FPS);
 
   char *spinners[] = {
@@ -23,8 +27,8 @@ int main(int argc, char **argv) {
       (char *)" - ▅ █ SSprite ", (char *)" \\ ▆ ▓ SSprite ",
       (char *)" | ▇ ▒ SSprite ", (char *)" / █ ░ SSprite ",
   };
+
   rgb_color spinners_color = {0x9C, 0x41, 0xdE}; // R, G, B
-  //
   rgb_palette *fade_palette = CreatePaletteFadeInOut(spinners_color, 256);
 
   TSRenderEngineTopDown engine;
@@ -74,49 +78,147 @@ int main(int argc, char **argv) {
   S1.frames[0]->color = S1Color;
   S1.background = &SubScreen.bg_color; // textcolor changes with scrn color
 
-  // -- main loop
-  int x = 0;
-  int y = 0;
-  int x2 = 0;
+  // NOTE: ---------------------------------------------------- VARS INIT ---
+  int x1 = 0; // for SpcShip1
+  int y1 = 0;
+  int x2 = 0; // for SprDemo
   int y2 = 0;
-  int x3 = 0;
+  int x3 = 0; // for SpcShip2
   int y3 = 0;
-  int x4 = 0;
+  int x4 = 0; // for SprTsprites
   int y4 = 0;
 
-  int direction = 0;
+  // Hide Spaceships outside
+  SpcShip1.SetXY(-100, -100);
+  SpcShip2.SetXY(-100, -100);
+  SpcShip3.SetXY(-100, -100);
+
+  SprTSprites.SetXY(10, 37 - 100);
+  SprDemo.SetXY(-100, 29);
+
+  // NOTE: ------------------------------------------------ FADES / MOVES ---
+
+  // -- SprTSprites  -------------------------------------- MOVE / FADE IN --
+  for (int i = 0; i < 100; i++) {
+    ts1 = get_timestamp(&tv);
+    dim_render_surface_in_out(SprTSprites.restore_surface, i, 99,
+                              SprTSprites.out_surface);
+    SprTSprites.SetXY(10, 37 - 100 + i);
+    Screen.Render();
+
+    ts2 = get_timestamp(&tv);
+    usleep(fps_in_us - (ts2 - ts1) < fps_in_us ? fps_in_us - (ts2 - ts1) : 1);
+  }
+  for (int i = 0; i < 60; i++) {
+    ts1 = get_timestamp(&tv); // start measuring frame time
+    dim_render_surface_in_out(SprTSprites.restore_surface, 100 - i, 99,
+                              SprTSprites.out_surface);
+    Screen.Render();
+    ts2 = get_timestamp(&tv);
+    usleep(fps_in_us - (ts2 - ts1) < fps_in_us ? fps_in_us - (ts2 - ts1) : 1);
+  }
+  for (int i = 0; i < 60; i++) {
+    ts1 = get_timestamp(&tv);
+    dim_render_surface_in_out(SprTSprites.restore_surface, 60 + i, 99,
+                              SprTSprites.out_surface);
+    Screen.Render();
+    ts2 = get_timestamp(&tv);
+    usleep(fps_in_us - (ts2 - ts1) < fps_in_us ? fps_in_us - (ts2 - ts1) : 1);
+  }
+
+  // -- SprDemo ------------------------------------------- MOVE / FADE IN --
+  for (int i = -100; i < 16; i++) {
+    ts1 = get_timestamp(&tv);
+    dim_render_surface_in_out(SprDemo.restore_surface, 116 + i, 116,
+                              SprDemo.out_surface);
+    SprDemo.SetXY(i, 29);
+    Screen.Render();
+
+    ts2 = get_timestamp(&tv);
+    usleep(fps_in_us - (ts2 - ts1) < fps_in_us ? fps_in_us - (ts2 - ts1) : 1);
+  }
+  // -- HIGHLIGHT
+  for (int j = 0; j < 2; j++) {
+    for (int i = 0; i < 60; i += 6) {
+      ts1 = get_timestamp(&tv);
+      dim_render_surface_in_out(SprDemo.restore_surface, 100 + i, 100,
+                                SprDemo.out_surface);
+      Screen.Render();
+      ts2 = get_timestamp(&tv);
+      usleep(fps_in_us - (ts2 - ts1) < fps_in_us ? fps_in_us - (ts2 - ts1) : 1);
+    }
+    for (int i = 0; i < 60; i += 6) {
+      ts1 = get_timestamp(&tv);
+      dim_render_surface_in_out(SprDemo.restore_surface, 160 - i, 100,
+                                SprDemo.out_surface);
+      Screen.Render();
+      ts2 = get_timestamp(&tv);
+      usleep(fps_in_us - (ts2 - ts1) < fps_in_us ? fps_in_us - (ts2 - ts1) : 1);
+    }
+  }
+  // --  FADE
+  for (int j = 0; j < 2; j++) {
+    for (int i = 0; i < 60; i++) {
+      ts1 = get_timestamp(&tv);
+      dim_render_surface_in_out(SprDemo.restore_surface, 100 - i, 99,
+                                SprDemo.out_surface);
+      Screen.Render();
+      ts2 = get_timestamp(&tv);
+      usleep(fps_in_us - (ts2 - ts1) < fps_in_us ? fps_in_us - (ts2 - ts1) : 1);
+    }
+    for (int i = 0; i < 60; i++) {
+      ts1 = get_timestamp(&tv);
+      dim_render_surface_in_out(SprDemo.restore_surface, 60 + i, 99,
+                                SprDemo.out_surface);
+      Screen.Render();
+      ts2 = get_timestamp(&tv);
+      usleep(fps_in_us - (ts2 - ts1) < fps_in_us ? fps_in_us - (ts2 - ts1) : 1);
+    }
+  }
+  // -- HIGHLIGHT
+  for (int j = 0; j < 2; j++) {
+    for (int i = 0; i < 60; i += 6) {
+      ts1 = get_timestamp(&tv);
+      dim_render_surface_in_out(SprDemo.restore_surface, 100 + i, 100,
+                                SprDemo.out_surface);
+      Screen.Render();
+      ts2 = get_timestamp(&tv);
+      usleep(fps_in_us - (ts2 - ts1) < fps_in_us ? fps_in_us - (ts2 - ts1) : 1);
+    }
+    for (int i = 0; i < 60; i += 6) {
+      ts1 = get_timestamp(&tv);
+      dim_render_surface_in_out(SprDemo.restore_surface, 160 - i, 100,
+                                SprDemo.out_surface);
+      Screen.Render();
+      ts2 = get_timestamp(&tv);
+      usleep(fps_in_us - (ts2 - ts1) < fps_in_us ? fps_in_us - (ts2 - ts1) : 1);
+    }
+  }
+
+  // NOTE: ------------------------------------------------------ MAIN LOOP ---
+
   while (1) {
     ts1 = get_timestamp(&tv); // start measuring frame time
     SpcShip1.counter1++;
 
-    x = 53 + 50 * (sin(((SpcShip1.counter1 % 100) / 100.0) * 6.28)) - 1;
-    y = 30 + 10 * (cos(((SpcShip1.counter1 % 100) / 100.0 * 3) * 6.28));
+    x1 = 53 + 50 * (sin(((SpcShip1.counter1 % 100 + 1) / 100.0) * 6.28)) - 1;
+    y1 = 30 + 10 * (cos(((SpcShip1.counter1 % 100 + 1) / 100.0 * 3) * 6.28));
 
-    x3 = 46 + 40 * (sin((((SpcShip1.counter1 + 50) % 100) / 100.0) * 6.28)) - 1;
-    y3 = 30 + 8 * (cos((((SpcShip1.counter1 + 50) % 100) / 100.0 * 3) * 6.28));
+    x2 = 5 + 25 * (sin(((SpcShip1.counter1 % 100 + 1) / 100.0) * 6.28)) + 10;
+    y2 = 20 + 10 * (cos(((SpcShip1.counter1 % 100 + 1) / 100.0) * 6.28));
 
-    x2 = 5 + 25 * (sin(((SpcShip1.counter1 % 100) / 100.0) * 6.28)) + 10;
-    y2 = 20 + 10 * (cos(((SpcShip1.counter1 % 100) / 100.0) * 6.28));
+    x3 = 46 +
+         40 * (sin((((SpcShip1.counter1 + 50 + 1) % 100) / 100.0) * 6.28)) - 1;
+    y3 = 30 +
+         8 * (cos((((SpcShip1.counter1 + 50 + 1) % 100) / 100.0 * 3) * 6.28));
 
-    x4 = 12 + 14 * (sin((((SpcShip1.counter1) % 100) / 100.0) * 6.28)) - 2;
+    x4 = 12 + 14 * (sin((((SpcShip1.counter1) % 100 + 1) / 100.0) * 6.28)) - 2;
     y4 = y3 + 15;
 
-    SpcShip1.SetXY(x, y);
+    SpcShip1.SetXY(x1, y1);
     SpcShip2.SetXY(x3, y3);
     SprDemo.SetXY(x2, y2);
     SprTSprites.SetXY(x4, y4);
-
-    // fade SprTsprites
-    if(!((SpcShip1.counter1 * 2) % 300)) direction = 1 - direction;
-    if (!direction)
-      dim_render_surface_in_out(SprTSprites.restore_surface,
-                                (SpcShip1.counter1 * 2) % 300 + 25, 200,
-                                SprTSprites.out_surface);
-    else
-      dim_render_surface_in_out(SprTSprites.restore_surface,
-                                300 - (SpcShip1.counter1 * 2 % 300) + 25, 200,
-                                SprTSprites.out_surface);
-
 
     // fade Spinner
     Spinner.SetColor(fade_palette->colors[SpcShip1.counter1 % 256]);
@@ -132,5 +234,6 @@ int main(int argc, char **argv) {
     usleep(fps_in_us - (ts2 - ts1) < fps_in_us ? fps_in_us - (ts2 - ts1) : 1);
   }
 
+  term_close();
   return 0;
 }
